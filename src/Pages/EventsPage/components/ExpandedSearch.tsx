bb095cfdffb18db39af02e1343835e366245ea5e
Ranjit Thota <amailtoranjith@gmail.com>
2023-10-10 08:08:49 +0200

    popup search colors

--- src/Pages/EventsPage/components/ExpandedSearch.tsx
+++ src/Pages/EventsPage/components/ExpandedSearch.tsx
@@ -1,32 +1,52 @@
+import React, { useState } from "react";
 import search from "../../../Images/search.svg";
 
 export default function ExpandedSearch({ handleExpandedClose }) {
+  const [date, setDate] = useState("");
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  const inputStyle = {
+    outline: "none",
+    width: "auto",
+    borderBottom: "1px solid black",
+    backgroundColor: "transparent",
+    color: date ? "black" : "#808080", // Change color to black if date is entered
+  };
+
   return (
     <div className="fixed top-[25vh] left-[10vw]">
-      <div className="flex flex-col items-center justify-center mx-auto mb-4 bg-white shadow-md border border-1  h-[50vh] w-[80vw] relative">
-        <div className="mt-6 mb-2 w-[90vw]  flex justify-center relative">
-          <div className="border border-1 rounded-lg border-black p-2  text-lg flex flex-col sm:flex-row">
+      <div className="flex flex-col items-center justify-center mx-auto mb-4 bg-white custom-box-shadow bg-gradient-to-r from-cyan-500 to-blue-500 border border-1 rounded-[20px]  h-[50vh] w-[80vw] relative">
+        <div className="mt-6 mb-2 w-[90vw]  flex justify-center relative ">
+          <div className="border border-1 rounded-lg border-black p-2 py-4 sm:py-2 text-lg flex flex-col sm:flex-row gap-8  sm:gap-6 max-w-[80%] bg-white">
             <input
               type="text"
               placeholder="Event Name"
-              className="w-fit outline-none w-[140px]"
+              className="outline-none w-[100px] border-b border-b-1 border-black bg-transparent placeholder-[#808080]"
             />
-
+            <span className="hidden sm:block">|</span>
             <input
               type="text"
               placeholder="City"
-              className="w-fit outline-none w-[140px]"
+              className="outline-none w-[100px] border-b border-b-1 border-black bg-transparent placeholder-[#808080]"
             />
+            <span className="hidden sm:block">|</span>
+
             <input
-              type="text"
+              type="Date"
               placeholder="Country"
-              className="w-fit outline-none w-[140px]"
+              className="outline-none w-auto border-b border-b-1 border-black bg-transparent "
+              style={inputStyle}
+              onChange={handleDateChange}
+              value={date}
             />
           </div>
         </div>
-        <div className="border border-1 rounded-full bg-black border-black p-2 shadow-md text-white flex w-fit cursor-pointer">
+        <div className=" rounded-full p-2 px-4 mt-2 shadow-md text-white flex w-fit cursor-pointer search-button">
           <p>Search</p>
-          <img src={search} alt="" className="w-6  " />
+          <img src={search} alt="" className="w-6  ml-2" />
         </div>
         <div
           className="bg-black text-white rounded-full px-4 py-2 absolute -top-2 -right-2 cursor-pointer ml-2"
--- src/Pages/EventsPage/components/TriggerSearch.tsx
+++ src/Pages/EventsPage/components/TriggerSearch.tsx
@@ -8,8 +8,8 @@ export default function TriggerSearch({ expanded, handleSearchClick }) {
     >
       <div className="mt-6 mb-2 w-[90vw] mr-0 sm:mr-[2.5vw] flex justify-center sm:justify-end cursor-pointer">
         <div className="border border-1 rounded-lg border-black pl-2 pr-1 py-1 max-w-[220px] text-lg flex items-center">
-          <p className="text-gray-400 mr-2">search an event</p>
-          <div className="border border-1 rounded-full bg-black border-black p-1 shadow-md">
+          <p className="text-gray-400 mr-2">Search</p>
+          <div className="rounded-full  p-1 shadow-md search-button">
             <img src={search} alt="" className="w-6 cursor-pointer " />
           </div>
         </div>
